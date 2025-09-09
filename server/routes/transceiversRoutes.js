23d5a11a1942c94a4d23934dc9a1fdb482f34502
Haldor Eliezer <calderonhaldor385@gmail.com>
2025-09-09 12:24:59 -0500

    Ruta de eliminar y editar

--- server/routes/transceiversRoutes.js
+++ server/routes/transceiversRoutes.js
@@ -1,7 +1,7 @@
 import {Router} from 'express'
 import Sfp from '../models/sfp.js';
 const router = Router()
-
+import mongoose from 'mongoose';
 
 const capitalizeFirstLetter = (string) => {
   if (typeof string !== 'string' || string.length === 0) {
@@ -69,4 +69,66 @@ router.get('/allTransceivers', async (req, res) => {
     });
   }
 });
+
+// PUT route to edit an existing SFP
+router.patch('/editTransceiver/:id', async (req, res) => {
+  const { id } = req.params;  
+  const updatedData = req.body;
+  console.log(updatedData)
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: 'Invalid SFP ID provided.' });
+    }
+
+    const sfpUpdated = await Sfp.findByIdAndUpdate(id, updatedData, {
+      new: true, 
+      runValidators: true
+    });
+
+    if (!sfpUpdated) {
+      return res.status(404).json({ message: 'SFP not found.' });
+    }
+
+    res.status(200).json({
+      message: 'SFP updated successfully!',
+      data: sfpUpdated
+    });
+
+  } catch (error) {
+    console.error('Error updating SFP:', error);
+    res.status(400).json({
+      message: 'Error updating SFP',
+      error: error.message
+    });
+  }
+});
+
+// DELETE route to delete an SFP
+router.delete('/deleteTransceiver/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: 'Invalid SFP ID provided.' });
+    }
+    const sfpDeleted = await Sfp.findByIdAndDelete(id);
+
+    if (!sfpDeleted) {
+      return res.status(404).json({ message: 'SFP not found.' });
+    }
+
+    res.status(200).json({
+      message: 'SFP deleted successfully!',
+      data: sfpDeleted
+    });
+
+  } catch (error) {
+    console.error('Error deleting SFP:', error);
+    res.status(500).json({
+      message: 'Error deleting SFP',
+      error: error.message
+    });
+  }
+});
+
 export default router;
